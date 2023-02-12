c48e7e37827029e1670e23bfe8b2b282980e5117
atao <87337334@qq.com>
2023-02-12 22:16:14 +0800

    梳理登录逻辑
    wx.getUserProfile接口获取登录弹窗改版了

--- pages/my/index.js
+++ pages/my/index.js
@@ -5,15 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userName: null,
+    userInfo: {}
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 点击登录
    */
-  onLoad: function (options) {
-
+  tapLogin: function () {
+    console.log("start----------")
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log("成功")
+      }
+    })
+    // 调用开放接口登录
+    wx.login({
+      success(res) {
+        if (res.code) {
+          console.log('登录成功')
+          //发起网络请求
+          // wx.request({
+          //   url: 'https://example.com/onLogin',
+          //   data: {
+          //     code: res.code
+          //   }
+          // })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -25,13 +50,14 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow(){
+  onShow() {
     if (typeof this.getTabBar === 'function' &&
       this.getTabBar()) {
       this.getTabBar().setData({
         active: 3
       })
-    }},
+    }
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
