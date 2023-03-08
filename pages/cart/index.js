22013c65dd267445189c6269d02ed7d89b979271
atao <87337334@qq.com>
2023-03-08 22:34:29 +0800

    feat:新增order页，修改cart页

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,11 +1,86 @@
 // pages/cart/index.js
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    skuid: ['1', '2'],
+    checkAll: true,
+    items: [
+      {value: 'USA', name: '美国'},
+      {value: 'CHN', name: '中国', checked: 'true'},
+      {value: 'BRA', name: '巴西'},
+      {value: 'JPN', name: '日本'},
+      {value: 'ENG', name: '英国'},
+      {value: 'FRA', name: '法国'}
+    ]
+  },
 
+  checkAll(e) {
+    // console.log(e.detail)
+    this.setData({
+      checkeAll: e.detail,
+    });
+   
+    const items = this.data.items
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      items[i].checked = e.detail
+    }
+    this.setData({
+      items
+    })
+  },
+
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    const items = this.data.items
+    const values = e.detail.value
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      items[i].checked = false
+
+      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (items[i].value === values[j]) {
+          items[i].checked = true
+          break
+        }
+      }
+    }
+
+    this.setData({
+      items
+    })
+  },
+
+  onChange(event) {
+    console.log(event.detail)
+    this.setData({
+      result: event.detail,
+    });
+  },
+
+  onClose(event) {
+    const { position, instance } = event.detail;
+    switch (position) {
+      case 'cell':
+        instance.close();
+        break;
+      case 'right':
+        Dialog.confirm({
+          message: '确定删除吗？',
+        }).then(() => {
+          instance.close();
+        });
+        break;
+    }
+  },
+
+  onClickButton(e) {
+    wx.navigateTo({
+      url: '/pages/order/index'
+    })
   },
 
   /**
